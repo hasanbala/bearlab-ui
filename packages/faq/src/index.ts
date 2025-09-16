2dd1bbf34c96dc95f154ee7d1b5d517fe13a1565
balahasan.35@gmail.com <balahasan.35@gmail.com>
2025-09-16 20:31:10 +0300

    edited faq package index file

--- packages/faq/src/index.ts
+++ packages/faq/src/index.ts
@@ -5,3 +5,5 @@ export { FaqV3 } from "./faqThree";
 export type { Props as FaqV1Props } from "./faqOne";
 export type { Props as FaqV2Props } from "./faqTwo";
 export type { Props as FaqV3Props } from "./faqThree";
+
+export { ICON_TYPE } from "./helpers";
